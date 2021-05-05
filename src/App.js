84aa9d3dc2fd97c982d40fe38fea9f57d535e846
omarhasni <hasniomar001@gmail.com>
2021-05-05 19:26:34 +0200

    exercice

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+import './App.css';
+import { Form,  Nav, Navbar } from 'react-bootstrap';
+function App() {
+  
+  return (
+    <div className="App">
+    
+    <Navbar bg="primary" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+  </Navbar>
+    
+    
+    <Form>
+    <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+    </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <button>Submit</button>
+</Form>
+    </div>
+  );
+}
+
+export default App;
